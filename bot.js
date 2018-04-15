6ba0bdf646d760713c4d1238f4fde5a642ee745f
yascript <38389189+yascript@users.noreply.github.com>
2018-04-14 23:52:21 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NDM0ODY5MzYzMTY2OTM3MDg4.DbRcQw.DQmokmCUAorMfy1qlos6xutCuEI);
